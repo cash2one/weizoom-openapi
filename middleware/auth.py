80c8976fad5d9cd583c3c47f015250b898721726
“chengdegong” <chengdegong@weizoom.com>
2016-09-28 21:13:56 +0800

    update auth delete logging info

--- middleware/auth.py
+++ middleware/auth.py
@@ -12,23 +12,23 @@ __author__ = 'bert'
 class Auth(object):
 	def process_request(self, req, res):
 		#TODO 将这部分放到 lua ＋ nginx中（或者kong中）
-		if '/auth/access_token' in req.path:
+		if '/auth/access_token' in req.path or 'console' in req.path:
 			return 
 		access_token = req.params.get('access_token', None)
-		if access_token:
+		if not access_token:
 			raise redirects.HTTPMiddlewareError( {
 				"errcode": 40003,
 				"errmsg": "invalid access_token"
 			})
 
-		app = cache_util.get_cache_wrapper(hashlib.md5(access_token).hexdigest())
+		app = cache_util.get_cache_wrapper(hashlib.md5(x).hexdigest())
 		if not app:
 			raise redirects.HTTPMiddlewareError( {
 				"errcode": 40003,
 				"errmsg": "invalid access_token"
 			})
 
-		req.context['woid'] = json.load(app)['woid']
-		req.context['api'] = json.load(app)['apiserver_access_token']
+		req.context['woid'] = json.loads(app)['woid']
+		req.context['apiserver_access_token'] = json.loads(app)['apiserver_access_token']
 		watchdog.info("auth:woid:%s" % req.context['woid'])
 		watchdog.info("auth:apiserver_access_token:%s" % req.context['apiserver_access_token'])
