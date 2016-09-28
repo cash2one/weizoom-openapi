a52cb2c4d9c59f630d7f8e4ef397b06ccca550b4
郭玉成 <guoyucheng@weizoom.com>
2016-09-28 18:41:00 +0800

    add access token

--- middleware/auth.py
+++ middleware/auth.py
@@ -5,12 +5,15 @@ import json
 
 from eaglet.core.cache import utils as cache_util
 from eaglet.core import redirects
+from eaglet.core import watchdog
 #from eaglet.core.cache import utils as cache_utils
 __author__ = 'bert'
 
 class Auth(object):
 	def process_request(self, req, res):
 		#TODO 将这部分放到 lua ＋ nginx中（或者kong中）
+		if '/auth/access_token' in req.path:
+			return 
 		access_token = req.params.get('access_token', None)
 		if access_token:
 			raise redirects.HTTPMiddlewareError( {
@@ -26,3 +29,6 @@ class Auth(object):
 			})
 
 		req.context['woid'] = json.load(app)['woid']
+		req.context['api'] = json.load(app)['apiserver_access_token']
+		watchdog.info("auth:woid:%s" % req.context['woid'])
+		watchdog.info("auth:apiserver_access_token:%s" % req.context['apiserver_access_token'])
