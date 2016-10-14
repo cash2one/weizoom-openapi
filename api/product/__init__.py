337a806e17124b7da697262f94ca6ed7d9864399
“chengdegong” <chengdegong@weizoom.com>
2016-10-14 17:00:21 +0800

    s

--- api/product/__init__.py
+++ api/product/__init__.py
@@ -2,4 +2,4 @@
 
 import a_product
 import a_products
-import a_product_catalogs
+import a_product_classifications
--- api/product/a_product_classification.py
+++ api/product/a_product_classification.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""@package db.notify.models
+"""@package api.product.a_product_classification
 通知信息表结构
 
 @author cdg
@@ -12,12 +12,12 @@ from eaglet.core import watchdog
 from eaglet.utils.resource_client import Resource
 from util.error_codes import *
 
-class AProductCatalogs(api_resource.ApiResource):
+class AProductClassification(api_resource.ApiResource):
     """
     商品分类信息
     API:
         method: get
-        url: mall/product_catalogs/
+        url: mall/product_classifications/
 
     Args:
         取值以及说明:
@@ -25,37 +25,37 @@ class AProductCatalogs(api_resource.ApiResource):
           product_id: 商品ID
     """
     app = "mall"
-    resource = "product_catalogs"
+    resource = "product_classifications"
 
     def get(args):
         try:
             data = {}
             param_data = {'access_token':args['apiserver_access_token']}
             resp = Resource.use('apiserver').get({
-                    'resource':'product.product_catalogs',
+                    'resource':'product.product_classifications',
                     'data':param_data
                 })
             if not resp or resp['code'] != 200:
-                data['errcode'] = FAIL_GET_PRODUCT_CATALOGS_CODE
-                data['errmsg'] = code2msg[FAIL_GET_PRODUCT_CATALOGS_CODE]
+                data['errcode'] = FAIL_GET_PRODUCT_CLASSIFICATIONS_CODE
+                data['errmsg'] = code2msg[FAIL_GET_PRODUCT_CLASSIFICATIONS_CODE]
                 return data
             #获取商品分类信息
             if resp['data']:
-                product_catalogs = resp['data']
+                product_classifications = resp['data']
 
-                catalog_level_1 = []
-                catalog_level_2 = []
+                classification_level_1 = []
+                classification_level_2 = []
                 #把商品的一级二级分类分成两个组
-                for catalog in product_catalogs:
-                    if catalog['level'] == 1:
-                        catalog_level_1.append(catalog)
-                    if catalog['level'] == 2:
-                        catalog_level_2.append(catalog)
+                for classification in product_classifications:
+                    if classification['level'] == 1:
+                        classification_level_1.append(classification)
+                    if classification['level'] == 2:
+                        classification_level_2.append(classification)
                 #组织分类信息数据结构
                 """
                 for example:
                     {
-                      'catalogs':[
+                      'classifications':[
                           {
                             'first_level_id':11,
                             'first_level_name':'衣服',
@@ -73,27 +73,27 @@ class AProductCatalogs(api_resource.ApiResource):
                     }
                 """
                 data = {}
-                catalogs = []
+                classifications = []
                 level1 = {}
                 level2 = []
                 level2_1 = {}
-                for catalog1 in catalog_level_1:
-                    level1['first_level_id'] = catalog1['id']
-                    level1['first_level_name'] = catalog1['name']
-                    for catalog2 in catalog_level_2:
-                        if catalog2['father_id'] == catalog1['id']:
-                            level2_1['second_level_id'] = catalog2['id']
-                            level2_1['second_level_name'] = catalog2['name']
+                for classification1 in classification_level_1:
+                    level1['first_level_id'] = classification1['id']
+                    level1['first_level_name'] = classification1['name']
+                    for classification2 in classification_level_2:
+                        if classification2['father_id'] == classification1['id']:
+                            level2_1['second_level_id'] = classification2['id']
+                            level2_1['second_level_name'] = classification2['name']
                             level2.append(level2_1)
                     level1['second_level_info'] = level2
-                    catalogs.append(level1)
+                    classifications.append(level1)
                     level2 = []
-                data['catalogs'] = catalogs
+                data['classifications'] = classifications
             else:
-                data['errcode'] = PRODUCT_CATALOGS_IS_NULL_CODE
-                data['errmsg'] = code2msg[PRODUCT_CATALOGS_IS_NULL_CODE]
+                data['errcode'] = PRODUCT_CLASSIFICATIONS_IS_NULL_CODE
+                data['errmsg'] = code2msg[PRODUCT_CLASSIFICATIONS_IS_NULL_CODE]
             return data
         except:
-            data['errcode'] = FAIL_GET_PRODUCT_CATALOGS_CODE
-            data['errmsg'] = code2msg[FAIL_GET_PRODUCT_CATALOGS_CODE]
+            data['errcode'] = FAIL_GET_PRODUCT_CLASSIFICATIONS_CODE
+            data['errmsg'] = code2msg[FAIL_GET_PRODUCT_CLASSIFICATIONS_CODE]
             return data
